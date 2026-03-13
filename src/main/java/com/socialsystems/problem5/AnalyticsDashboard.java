71cb54ee28dd5cc79dd9e74b2f3f23b8ab55ecd6
NiranjanaKrishnaK <niranjanakumar2510@gmail.com>
2026-03-13 08:58:53 +0530

    Implemented Problem 5 AnalyticsDashboard with user input

--- src/main/java/com/socialsystems/problem5/AnalyticsDashboard.java
+++ src/main/java/com/socialsystems/problem5/AnalyticsDashboard.java
@@ -1,4 +1,29 @@
 package com.socialsystems.problem5;
 
+import java.util.*;
+
 public class AnalyticsDashboard {
+    private List<Integer> data = new ArrayList<>();
+
+    public void addData(int value) {
+        data.add(value);
+    }
+
+    public double average() {
+        return data.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AnalyticsDashboard dashboard = new AnalyticsDashboard();
+
+        System.out.print("Enter number of data points: ");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter value: ");
+            dashboard.addData(sc.nextInt());
+        }
+
+        System.out.println("Average: " + dashboard.average());
+    }
 }
