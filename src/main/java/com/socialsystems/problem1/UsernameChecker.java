4f050c5417899d739b3000e1f127e7ead9837b85
NiranjanaKrishnaK <niranjanakumar2510@gmail.com>
2026-03-13 08:58:10 +0530

    Implemented Problem 1 UsernameChecker with user input

--- src/main/java/com/socialsystems/problem1/UsernameChecker.java
+++ src/main/java/com/socialsystems/problem1/UsernameChecker.java
@@ -1,4 +1,18 @@
 package com.socialsystems.problem1;
 
+import java.util.Scanner;
+
 public class UsernameChecker {
+    public boolean isValidUsername(String username) {
+        return username != null && username.length() >= 3;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a username: ");
+        String username = sc.nextLine();
+
+        UsernameChecker checker = new UsernameChecker();
+        System.out.println("Valid? " + checker.isValidUsername(username));
+    }
 }
